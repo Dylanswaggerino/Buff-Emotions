6462a6c16075bdb7b31829bf9906233b2a782675
Dylanswaggerino <Dylanswaggerino@users.noreply.github.com>
2018-02-18 17:14:52 +0100

    Initial commit
    oof

---
+++ Buff Emoticons/BuffEmoticon.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.IO.Streams;
+
+using Terraria;
+using Terraria.ID;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
+
+namespace ClassLibrary3
+{
+    [ApiVersion(1, 21)]
+    public class Class1 : TerrariaPlugin
+    {
+
+        public override Version Version
+        {
+            get { return new Version("1.3"); }
+        }
+
+        public override string Name
+        {
+            get { return "Buff Emoticons."; }
+        }
+
+        public override string Author
+        {
+            get { return "Dylan"; }
+        }
+
+        public Class1(Main game)
+            : base(game)
+        {
+            Order = 4;
+        }
+        public override string Description
+        {
+            get { return "This Plugin give you a Buff when you type a command like /sad u should be get a buff with sad!"; }
+        }
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command(Love, "love"));
+            Commands.ChatCommands.Add(new Command(Mad, "mad"));
+            Commands.ChatCommands.Add(new Command(Happy, "happy"));
+            Commands.ChatCommands.Add(new Command(Confused, "confused"));
+            Commands.ChatCommands.Add(new Command(Sad, "sad"));
+            Commands.ChatCommands.Add(new Command(Dreaming, "dream"));
+            Commands.ChatCommands.Add(new Command(Freedom, "freedom"));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+        public void Love(CommandArgs e)
+        {
+            e.Player.SetBuff(119, 2600, true);
+        }
+        public void Mad(CommandArgs e)
+        {
+            e.Player.SetBuff(115, 2600, true);
+        }
+        public void Happy(CommandArgs e)
+        {
+            e.Player.SetBuff(146, 2600, true);
+        }
+        public void Confused(CommandArgs e)
+        {
+            e.Player.SetBuff(31, 2600, true);
+        }
+        public void Sad(CommandArgs e)
+        {
+            e.Player.SetBuff(103, 2600, true);
+        }
+        public void Dreaming(CommandArgs e)
+        {
+            e.Player.SetBuff(128, 2600, true);
+        }
+	    public void Freedom(CommandArgs e)
+        {
+            e.Player.SetBuff(298, 2600, true); // First Buff ID //Second Time In MilliSeconds
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Buff
+Buff Emoticons
+Buff Emoticons are Emoticons That gives players a Emoticon when he typed a command like /sad (newest command) 
+
+
+
+Ⓘⓝⓢⓣⓐⓛⓛⓐⓣⓘⓞⓝ ⓖⓤⓘⓓⓔ
+1. ⓒⓞⓟⓨ Ⓣⓗⓔ ⒹⓁⓁ ⓕⓘⓛⓔ Ⓐⓝⓓ Ⓟⓐⓢⓣⓔ ⓘⓣ
+2. ⒹⓄⓃⒺ
